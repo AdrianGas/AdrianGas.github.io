7a9b4f22503225355d7d7bbc21d274683583bc71
Adrian <adrian.gas.h@hotmail.com>
2020-02-23 16:29:37 -0500

    agregamos Java, Css e imagenes

---
+++ data.js
@@ -0,0 +1,58 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-equirectangular-the-wesley",
+      "name": "Equirectangular THE WESLEY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1157248661106074,
+          "pitch": -0.010648954721784065,
+          "rotation": 25.132741228718363,
+          "target": "0-equirectangular-the-wesley"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3456086303162458,
+          "pitch": -0.22808850639830425,
+          "title": "The wesley",
+          "text": "Shh... El film está por empezar en <b>THE CINEMA CLUB.</b>"
+        },
+        {
+          "yaw": -0.8096842685176888,
+          "pitch": 0.20757655481442683,
+          "title": "THE CINEMA CLUB",
+          "text": "¿Palomitas? Sírvete y disfruta del film en&nbsp;<b>THE CINEMA CLUB</b><div><b><br></b><div><div><b>Una de las amenidades disponibles para los residentes de THE WESLEY</b></div></div></div>"
+        }
+      ]
+    }
+  ],
+  "name": "THE CINEMA CLUB",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
